984cfa89e896a6736be683a26c470e17613bc056
Andrii Kozin <ankozin@microsoft.com>
2017-04-10 22:05:42 +0200

    Add firebase module

--- sport-journal/src/app/app.component.ts
+++ sport-journal/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from "angularfire2";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items: FirebaseListObservable<any[]>;
   title = 'app works!';
+  
+  constructor(angularFireBase: AngularFire) {
+    this.items = angularFireBase.database.list('/items');
+  }
 }
