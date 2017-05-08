85b9a7e3141c2fad2913f9848d5e3dda5cfef318
Andrii Kozin <z4i.andrey@gmail.com>
2017-05-08 09:57:15 +0200

    Generate junit xml report for unit tests

--- sport-journal/karma.conf.js
+++ sport-journal/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
@@ -33,7 +34,16 @@ module.exports = function (config) {
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+              : ['progress', 'junit'],
+    junitReporter: {
+      outputDir: 'testResults', // results will be saved as $outputDir/$browserName.xml 
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile 
+      suite: '', // suite will become the package name attribute in xml testsuite element 
+      useBrowserName: false, // add browser name to report and classes names 
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element 
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element 
+      properties: {} // key value pair of properties to add to the <properties> section of the report 
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
