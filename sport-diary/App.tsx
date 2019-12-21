a42923ad9ee112a55023b950650ce6d5ab5f7ca7
Andrii Kozin <ankozin@microsoft.com>
2019-12-21 23:04:59 +0100

    Attempt to understand RN

--- sport-diary/App.tsx
+++ sport-diary/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+function UselessTextInput(props) {
+  return (
+    <TextInput
+      {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+      editable
+      maxLength={40}
+    />
+  );
+}
 
 export default function App() {
+  const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <UselessTextInput
+        multiline
+        numberOfLines={4}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
     </View>
   );
 }
